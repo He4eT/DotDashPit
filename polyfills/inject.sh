b13f46b57a8595b655c66d7f79ca226bf0d8d7a7
He4eT <He4eT@oddsquat.org>
2025-06-24 14:37:31 +0200

    es3: change config

--- polyfills/inject.sh
+++ polyfills/inject.sh
@@ -5,6 +5,8 @@ GAME_FILE="game.js"
 TEMP_FILE="merged_game.js"
 
 sed -i 's/TIC()/_()/' "$GAME_FILE"
+sed -i 's/FONT_SIZE = 1$/FONT_SIZE = 2/' "$GAME_FILE"
+sed -i 's/DOT_DASH_THRESHOLD = 200$/DOT_DASH_THRESHOLD = 150/' "$GAME_FILE"
 
 cat "$POLYFILLS_FILE" "$GAME_FILE" > "$TEMP_FILE"
 mv "$TEMP_FILE" "$GAME_FILE"
